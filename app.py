9391017c6f0d985abfd7bf4de10e6fc00e1ab400
Jaime Deng <jaime@jaimes-mbp.wifi.bcit.ca>
2020-03-06 15:28:55 -0800

    show static file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/chris')
 def index():
-    return "test"
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run(port=9999, debug=True)
